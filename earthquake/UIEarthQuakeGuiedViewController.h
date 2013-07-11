4f4048229f766100a13ccbc81a5ac8c2cb343705
Wu Yinghao <wyh817@gmail.com>
2013-07-11 14:29:55 +0800

    修改了tableviewcell
    修改了tableviewcell，之前的版本有问题会造出闪退
    添加了earthquake guid 页面，该页面暂时为空

---
+++ earthquake/UIEarthQuakeGuiedViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIEarthQuakeGuiedViewController.h
+//  earthquake
+//
+//  Created by wu yinghao on 13-7-11.
+//  Copyright (c) 2013年 wu yinghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIEarthQuakeGuiedViewController : UIViewController
+
+@end
---
+++ earthquake/UIEarthQuakeGuiedViewController.m
@@ -0,0 +1,38 @@
+//
+//  UIEarthQuakeGuiedViewController.m
+//  earthquake
+//
+//  Created by wu yinghao on 13-7-11.
+//  Copyright (c) 2013年 wu yinghao. All rights reserved.
+//
+
+#import "UIEarthQuakeGuiedViewController.h"
+
+@interface UIEarthQuakeGuiedViewController ()
+
+@end
+
+@implementation UIEarthQuakeGuiedViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
